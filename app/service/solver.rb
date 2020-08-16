509140853c893b5fd635c665e49a40950f3662da
bautiblacker <bblacker@itba.edu.ar>
2020-08-15 23:14:12 -0300

    solver validations added

--- app/service/solver.rb
+++ app/service/solver.rb
@@ -5,11 +5,12 @@ require 'json'.freeze
 
 class Solver
   class RequestError < StandardError; end
+  class ParsingError < StandardError; end
 
   BASE_URL = 'https://mine-sweeper-generator.herokuapp.com/solver'.freeze
 
   def self.call
-    escaped_address = URI.URI.encode_www_form(BASE_URL)
+    escaped_address = URI.escape(BASE_URL)
     uri = URI.parse(escaped_address)
     response = Net::HTTP.get_response(uri)
     raise RequestError.new, 'Bad Request' unless response.code == '200'
@@ -19,16 +20,21 @@ class Solver
 
   def self.handle_response_body(response)
     minesweeper = JSON.parse(response)
+    raise ParsingError unless minesweeper.class == Array
+
     problem = minesweeper.map(&:clone)
     answer = solve(minesweeper)
     { problem: problem, answer: answer }
   end
 
+  private
+
   def self.solve(matrix)
-    matrix_size = matrix.size
-    unless matrix_size.zero?
-      (0...matrix.size).each do |row_index|
-        (0...matrix.size).each do |col_index|
+    rows_number = matrix.size
+    unless rows_number.zero?
+      cols_number = matrix[0].size
+      (0...rows_number).each do |row_index|
+        (0...cols_number).each do |col_index|
           matrix[row_index][col_index] = count_mines(row_index, col_index, matrix) if matrix[row_index][col_index] == ' ' # rubocop:disable Layout/LineLength: Line is too long
         end
       end
@@ -50,6 +56,8 @@ class Solver
   end
 
   def self.validate_indexes(row_index, col_index, matrix)
-    row_index >= 0 && col_index >= 0 && row_index < matrix.size && col_index < matrix.size
+    rows_number = matrix.size
+    cols_number = matrix[0].size
+    row_index >= 0 && col_index >= 0 && row_index < rows_number && col_index <cols_number
   end
 end
